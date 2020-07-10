3df665e2faf127eb4a00d24e0223c9901c488ecd
Taniya <tanu2599@gmail.com>
2020-07-10 13:22:25 +0530

    Added to py file

--- stock-predict.py
+++ stock-predict.py
@@ -2,3 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+data = pd.read_csv("./Dataset/BanksDataset/AXISBANK.csv")
+data.head(10)
+data.drop(["Symbol", "Series","Trades","Deliverable Volume","%Deliverble"], axis= 1)
